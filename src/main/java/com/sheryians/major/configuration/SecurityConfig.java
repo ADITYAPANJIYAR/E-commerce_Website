b137f38e9bac6328d7da3ba6aed8a68fd9178f5f
Aditya Panjiyar <aditya.8mi@gmail.com>
2023-09-05 01:04:10 +0530

    Fixed Images in SecurityConfig.java

--- src/main/java/com/sheryians/major/configuration/SecurityConfig.java
+++ src/main/java/com/sheryians/major/configuration/SecurityConfig.java
@@ -66,6 +66,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**", "/images/**", "/productimages/**", "/css/**", "/js/**");
+        web.ignoring().antMatchers("/resources/**", "/images/**", "/productImages/**", "/css/**", "/js/**");
     }
 }
